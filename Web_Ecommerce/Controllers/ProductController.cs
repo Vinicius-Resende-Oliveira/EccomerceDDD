2dfe8bb006d633d5d372b0510a180770a3f8e4de
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2022-02-03 00:08:47 -0300

    Criando as view  e controllers da aplicação web

---
+++ Web_Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using ApplicationApp.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Ecommerce.Controllers
+{
+ 
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public readonly InterfaceProduct _InterfaceProduct;
+        public ProductController(InterfaceProduct InterfaceProduct)
+        {
+            _InterfaceProduct = InterfaceProduct;
+        }
+
+        // GET: ProductController
+        public async Task<IActionResult> Index()
+        {
+            return View(await _InterfaceProduct.List());
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _InterfaceProduct.GetEntityById(id));
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            try
+            {
+                await _InterfaceProduct.AddProduct(product);
+                if (product.Notitycoes.Any())
+                {
+                    foreach (var item in product.Notitycoes)
+                    {
+                        ModelState.AddModelError(item.NomePropriedade, item.Message);
+                    }
+                    return View("Edit", product);
+                }
+            }
+            catch
+            {
+                return View("Edit", product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await _InterfaceProduct.GetEntityById(id));
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            try
+            {
+                await _InterfaceProduct.UpdateProduct(product);
+                if (product.Notitycoes.Any())
+                {
+                    foreach (var item in product.Notitycoes)
+                    {
+                        ModelState.AddModelError(item.NomePropriedade, item.Message);
+                    }
+                    return View("Edit", product);
+                }
+            }
+            catch
+            {
+                return View("Edit", product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ProductController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _InterfaceProduct.GetEntityById(id));
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Product product)
+        {
+            try
+            {
+                var productDelete = await _InterfaceProduct.GetEntityById(id);
+
+                await _InterfaceProduct.Delete(productDelete);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Web_Ecommerce/Startup.cs
+++ Web_Ecommerce/Startup.cs
@@ -1,4 +1,12 @@
+using ApplicationApp.Interfaces;
+using ApplicationApp.OpenApp;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Services;
 using Infrastructure.Configurations;
+using Infrastructure.Repository.Generics;
+using Infrastructure.Repository.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,6 +43,16 @@ namespace Web_Ecommerce
                 .AddEntityFrameworkStores<ContextBase>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            // INTERFACE E REPOSITÓRIO
+            services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGeneric<>));
+            services.AddSingleton<IProduct, RepositoryProduct>();
+
+            // INTERFACE APLICAÇÃO
+            services.AddSingleton<InterfaceProduct, AppProduct>();
+            
+            // INTERFACE SERVIÇÕS
+            services.AddSingleton<IServiceProduct, ServiceProduct>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
